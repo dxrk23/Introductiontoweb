0acbf1ce90b90ef11e89c1b85bdfc766c9b7cba6
Dimash <dimab797@gmail.com>
2020-01-27 22:24:18 +0600

    26.01

---
+++ index.js
@@ -0,0 +1,6 @@
+function kakegenre(){
+  document.getElementById('test1').innerHTML = "Drama Games Psychology";
+}
+function nevestagenre(){
+  document.getElementById('test2').innerHTML = "Magic Fantesy";
+}
---
+++ playy.js
@@ -0,0 +1,5 @@
+function function1(){
+  // document.getElementById("para").innerHTML = "SOSI";
+  // document.getElementById("para").innerHTML = "Paragraph changed.";
+  // document.getElementsByClassName("drop").classList.toggle("drop");
+}
