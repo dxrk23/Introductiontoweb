ac3bfd8edfc66e851c9236c4b7bcb0b958412c2d
Dimash <dimab797@gmail.com>
2020-01-30 11:23:28 +0600

    js updated

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
+var nevestacounter = 1 , kakecounter = 1;
+
 function kakegenre(){
-  let a = document.getElementById('test1').innerHTML;
-  if (a == " " || a == null){
-    document.getElementById('test1').innerHTML = "Drama Games Psychology";
-    return 0;
+  switch (kakecounter) {
+    case 0:
+      document.getElementById('test1').innerHTML = " ";
+      break;
+    case 1:
+      document.getElementById('test1').innerHTML = "Drama Games Psychology";
+      break;
+    case 2:
+      document.getElementById('test1').innerHTML = "Anime about gambling in high school.";
+      break;
   }
-  else{
-    document.getElementById('test1').innerHTML = " ";
-    return 0;
+  if (kakecounter == 2){
+    kakecounter = 0;
   }
+  else ++kakecounter;
+  return 0;
 }
 
 function nevestagenre(){
-  let a = document.getElementById('test2').innerHTML;
-  if (a == " " || a == null){
-    document.getElementById('test2').innerHTML = "Magic Fantesy";
-    return 0;
+
+  switch (nevestacounter) {
+    case 0:
+      document.getElementById('test2').innerHTML = " ";
+      break;
+    case 1:
+      document.getElementById('test2').innerHTML = "Magic Fantesy";
+      break;
+    case 2:
+      document.getElementById('test2').innerHTML = "Anime about gambling in high school.";
+      break;
   }
-  else{
-    document.getElementById('test2').innerHTML = " ";
-    return 0;
+  if (nevestacounter == 2){
+    nevestacounter = 0;
   }
+  else ++nevestacounter;
+  return 0;
+
 }
