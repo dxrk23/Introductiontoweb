3e357055d80831848db30362cc3ac20668285bbd
Dimash <dimab797@gmail.com>
2020-01-30 11:14:39 +0600

    updated js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function kakegenre(){
-  document.getElementById('test1').innerHTML = "Drama Games Psychology";
+  let a = document.getElementById('test1').innerHTML;
+  if (a == " " || a == null){
+    document.getElementById('test1').innerHTML = "Drama Games Psychology";
+    return 0;
+  }
+  else{
+    document.getElementById('test1').innerHTML = " ";
+    return 0;
+  }
 }
+
 function nevestagenre(){
-  document.getElementById('test2').innerHTML = "Magic Fantesy";
+  let a = document.getElementById('test2').innerHTML;
+  if (a == " " || a == null){
+    document.getElementById('test2').innerHTML = "Magic Fantesy";
+    return 0;
+  }
+  else{
+    document.getElementById('test2').innerHTML = " ";
+    return 0;
+  }
 }
