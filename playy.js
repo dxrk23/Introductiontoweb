7a50d8a4fcf1430f527396eb84eca5f2dd4aaed9
Dimash <dimab797@gmail.com>
2020-02-10 19:09:46 +0600

    lol

--- playy.js
+++
@@ -1,5 +0,0 @@
-function function1(){
-  // document.getElementById("para").innerHTML = "SOSI";
-  // document.getElementById("para").innerHTML = "Paragraph changed.";
-  // document.getElementsByClassName("drop").classList.toggle("drop");
-}
