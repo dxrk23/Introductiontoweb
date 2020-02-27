45404bafb50b6a37c6c27c727a103ebc9e8ba9cf
Dimash <dimab797@gmail.com>
2020-02-28 01:31:16 +0600

    28.02

---
+++ js/script.js
@@ -0,0 +1,87 @@
+function logocheck() {
+  let x = document.querySelector(".forma1").value;
+  let kcell = document.createElement("img");
+  let beeline = document.createElement("img");
+  let tele2 = document.createElement("img");
+  document.querySelector(".celllogo").innerHTML = "";
+  kcell.src = "logos/kcell.png";
+  beeline.src = "logos/beeline.png";
+  tele2.src = "logos/tele2.png";
+  let src = document.querySelector(".celllogo");
+
+  //kcell
+  if (x.includes('+7701')) {
+    src.appendChild(kcell);
+  }
+  if (x.includes('+7702')) {
+    src.appendChild(kcell);
+  }
+  if (x.includes('+7775')) {
+    src.appendChild(kcell);
+  }
+  if (x.includes('+7778')) {
+    src.appendChild(kcell);
+  }
+
+  //beeline
+  if (x.includes('+7777')) {
+    src.appendChild(beeline);
+  }
+  if (x.includes('+7776')) {
+    src.appendChild(beeline);
+  }
+  if (x.includes('+7705')) {
+    src.appendChild(beeline);
+  }
+
+  //tele2
+  if (x.includes('+7747')) {
+    src.appendChild(tele2);
+  }
+  if (x.includes('+7707')) {
+    src.appendChild(tele2);
+  }
+}
+
+function validation() {
+  Swal.fire({
+    icon: 'success',
+    title: 'transaction passed',
+    text: 'In few minutes your balance should popolnitsay!',
+    footer: '<a href>One more operation?</a>'
+  });
+}
+
+function tariffcheck() {
+  document.querySelector(".tarif1").style.display = 'none';
+  document.querySelector(".tarif2").style.display = 'none';
+  document.querySelector(".tarif3").style.display = 'none';
+  document.querySelector(".tarif4").style.display = 'none';
+  document.querySelector(".tarif5").style.display = 'none';
+  document.querySelector(".tarif6").style.display = 'none';
+  document.querySelector(".tarif7").style.display = 'none';
+  document.querySelector(".tarif8").style.display = 'none';
+  let x = document.querySelector(".naskolkobogat").value;
+  if (x >= 1390) {
+    document.querySelector(".tarif7").style.display = 'block';
+  }
+  if (x >= 1890) {
+    document.querySelector(".tarif8").style.display = 'block';
+  }
+  if (x >= 1990) {
+    document.querySelector(".tarif1").style.display = 'block';
+  }
+  if (x >= 2590) {
+    document.querySelector(".tarif2").style.display = 'block';
+    document.querySelector(".tarif4").style.display = 'block';
+  }
+  if (x >= 2990) {
+    document.querySelector(".tarif3").style.display = 'block';
+  }
+  if (x >= 3590) {
+    document.querySelector(".tarif5").style.display = 'block';
+  }
+  if (x >= 5590) {
+    document.querySelector(".tarif6").style.display = 'block';
+  }
+}
