e0769c0598d21fbe01b2ad4bee0748e88eef1d60
Yedil Dinmukhamed <54952217+dxrk23@users.noreply.github.com>
2020-03-03 18:54:19 +0600

    03.03

--- Progress 8/index.js
+++ Progress 8/index.js
@@ -25,3 +25,70 @@ function rotate() {
       document.getElementById("__second_line").style.flexDirection = 'row';
     }
 }
+
+function Style1(){
+  let x = document.getElementsByClassName('__white');
+  let y = document.getElementsByClassName('__black');
+
+  for (let i = 0; i < 32; i++) {
+    x[i].style.backgroundColor = 'white';
+    y[i].style.backgroundColor = 'black';
+  }
+    document.querySelector('.__border').style.border = '100px solid silver'
+}
+
+function Style2(){
+  try{
+    let x = document.getElementsByClassName('__whitee');
+    let y = document.getElementsByClassName('__blac');
+
+    for (let i = 0; i < 32; i++) {
+      x[i].style.backgroundColor = 'D6F599';
+      y[i].style.backgroundColor = '436436';
+  }
+  document.querySelector('.__border').style.border = '100px solid D2FF28'
+  } catch(err) {
+    throw new err;
+  } finally {
+    console.log(" Style was chaged")
+  }
+}
+
+function Style3(){
+  let x = document.getElementsByClassName('__white');
+  let y = document.getElementsByClassName('__black');
+
+  for (let i = 0; i < 32; i++) {
+    x[i].style.backgroundColor = '#EDCBB1';
+    y[i].style.backgroundColor = '#B7245C';
+  }
+  document.querySelector('.__border').style.border = '100px solid #7C3238'
+}
+
+function Style4(){
+  let x = document.getElementsByClassName('__white');
+  let y = document.getElementsByClassName('__black');
+
+  for (let i = 0; i < 32; i++) {
+    x[i].style.backgroundColor = '#E0B7B7';
+    y[i].style.backgroundColor = '#DC7F9B';
+  }
+  document.querySelector('.__border').style.border = '100px solid #94BFA7'
+}
+
+function Style5(){
+  try{
+    let x = document.getElementsByClassName('__white');
+    let y = document.getElementsByClassName('__black');
+
+    for (let j = 0; i < 32; i++) {
+      x[i].style.backgroundColor = 'D6F599';
+      y[i].style.backgroundColor = '436436';
+  }
+  document.querySelector('.__border').style.border = '100px solid D2FF28'
+  } catch(err) {
+    throw new err;
+  } finally {
+    console.log(" Style was chaged")
+  }
+}
